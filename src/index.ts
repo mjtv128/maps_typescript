dd08058040da131878d4adb29142cd39bad7d927
Michelle Van <mjtvan@gmail.com>
2020-04-21 12:35:42 +0100

    added google maps

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-import {User} from './User'
-import {Company} from './Company'
+// import {User} from './User'
+// import {Company} from './Company'
 
-const user = new User()
-const company = new Company()
+// const user = new User()
+// const company = new Company()
 
-console.log(company)
-console.log(user)
+// console.log(company)
+// console.log(user)
+
+//create html element mapdiv to house the google map
+
+//create an instance of map class
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+})
